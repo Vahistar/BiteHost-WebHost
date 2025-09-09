e0c1e25c3ba0861baa0e7db28236b5ecb73ec5d3
Bartosz "Vahistar <41486486+Vahistar@users.noreply.github.com>
2025-09-09 05:30:26 -0700

    Add files via upload

---
+++ webroot/index.php
@@ -0,0 +1,16 @@
+<?php
+// Get the PHP version
+$phpVersion = phpversion();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Version Info</title>
+</head>
+<body>
+PHP: <?php echo $phpVersion; ?>
+</body>
+</html>
